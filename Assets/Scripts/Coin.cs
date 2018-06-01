8bc60cdb327bfd54fdcb701f146a3b454a504dcd
ayaulymalzhan <ayaulymalzhan@gmail.com>
2018-06-01 22:03:29 +0600

    Initial commit

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+	public PlayerMover pm;
+	void onTriggerEnter(Collider col){
+		print ("Entered");
+		pm.coin++;
+		//Destroy(this.gameObject);
+
+		if (col.gameObject.CompareTag ("Coin")) {
+			col.gameObject.SetActive (false);
+		}
+
+
+	}
+}
---
+++ Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMover : MonoBehaviour {
+	public float speed=0.5f;
+	public int coin = 0;
+
+	Rigidbody rb;
+	private Vector3 startPos;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		startPos = transform.position;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate (){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (-moveHorizontal, 0, -moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+
+
+}
